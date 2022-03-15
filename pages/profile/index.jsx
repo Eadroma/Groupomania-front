6cdeff589ca9aec522622b372e0de2775ed3f85b
Eadroma <boukottaya.jr@gmail.com>
2022-03-15 15:01:03 +0100

    [ADD] Header Layout profile page

---
+++ pages/profile/index.jsx
@@ -0,0 +1,50 @@
+import { HeaderModule } from "../../components/Header";
+import { NavbarModule } from "../../components/Navbar/Navbar";
+import { getDataAPI } from "../../components/UserProvider";
+import { FooterModule } from "../../components/Footer/Footer";
+import styles from "./Profile.module.css";
+import { useEffect, useState } from "react";
+export default function Home() {
+  const [data, setData] = useState({});
+  useEffect(async () => {
+    let user = JSON.parse(localStorage.getItem("user"));
+    const fetchData = await getDataAPI(user.id);
+    setData(fetchData);
+  }, []);
+
+  return (
+    <div className={styles.container} id="container">
+      <HeaderModule title="Groupomania" description="None yet" />
+      <NavbarModule />
+      <div class="card">
+        <div class="card-image">
+          <figure class="image is-1080x275">
+            <img
+              src="https://caltrout.org/wp-content/uploads/2016/02/header-placeholder.jpg"
+              alt="Placeholder image"
+            />
+          </figure>
+        </div>
+        <div className={styles.cardHeader}>
+          <div class="card-image" className={styles.avatarImg}>
+            <figure class="image is-128x128">
+              <img
+                class="is-rounded"
+                src="https://image.shutterstock.com/image-vector/user-account-circle-profile-line-600w-272552858.jpg"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4 has-text-centered">{data.email.split("@")[0]}</p>
+                <p class="subtitle is-6 has-text-centered">{data.email}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterModule />
+    </div>
+  );
+}
